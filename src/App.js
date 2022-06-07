5679f02b39090923b130f2003daca550b8e7b994
unknown <imjonathan.work@gmail.com>
2022-06-06 20:40:54 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import DisplayList from "./components/DisplayList";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    getCoins();
+  }, []);
+
+  const getCoins = async () => {
+    const result = await fetch("https://api.coincap.io/v2/assets?limit=20");
+    let data = await result.json();
+    setCoins(data);
+  };
+  console.log(coins);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <DisplayList />
       </header>
     </div>
   );
---
+++ src/components/DisplayList.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const DisplayList = () => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>name</th>
+            <th>name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>name</td>
+            <td>name</td>
+            <td>name</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default DisplayList;
